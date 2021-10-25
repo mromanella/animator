a59857f3313a046d60e221767f7abe98ed58e913
mromanella <mroms21@gmail.com>
2021-10-25 10:19:50 -0400

    Fix collision detection

--- animator/src/collisions.ts
+++ animator/src/collisions.ts
@@ -1,37 +1,30 @@
 import { GameObject } from "./objects/index";
 
-function _withinHorizontal(obj1: GameObject, obj2: GameObject): boolean {
-    const obj1Bounds = obj1.getBoundingBox();
-    const obj2Bounds = obj2.getBoundingBox();
-
-    if (obj2Bounds.xMin <= obj1Bounds.xMax && obj2Bounds.xMin >= obj1Bounds.xMin) {
-        return true;
-    }
-    return false;
-}
-
-function _withinVertical(obj1: GameObject, obj2: GameObject): boolean {
-    const obj1Bounds = obj1.getBoundingBox();
-    const obj2Bounds = obj2.getBoundingBox();
-
-    if (obj2Bounds.yMin <= obj1Bounds.yMax && obj2Bounds.yMin >= obj1Bounds.yMin) {
-        return true;
-    }
-    return false;
-}
 
 function withinHorizontal(obj1: GameObject, obj2: GameObject): boolean {
-    return _withinHorizontal(obj1, obj2) || _withinHorizontal(obj2, obj1);
+    const obj1Bounds = obj1.getBoundingBox();
+    const obj2Bounds = obj2.getBoundingBox();
+
+    if (obj1Bounds.xMin < obj2Bounds.xMax && obj1Bounds.xMax > obj2Bounds.xMin) {
+        return true;
+    }
+    return false;
 }
 
 function withinVertical(obj1: GameObject, obj2: GameObject) {
-    return _withinVertical(obj1, obj2) || _withinVertical(obj2, obj1);
+    const obj1Bounds = obj1.getBoundingBox();
+    const obj2Bounds = obj2.getBoundingBox();
+
+    if (obj1Bounds.yMin < obj2Bounds.yMax && obj1Bounds.yMax > obj2Bounds.yMin) {
+        return true;
+    }
+    return false;
 }
 
 function checkCollision(obj1: GameObject, obj2: GameObject): boolean {
 
-    const horiz = withinHorizontal(obj1, obj2) || withinHorizontal(obj2, obj1);
-    const vert = withinVertical(obj1, obj2) || withinVertical(obj2, obj1);
+    const horiz = withinHorizontal(obj1, obj2);
+    const vert = withinVertical(obj1, obj2);
 
     return horiz && vert;
 }
